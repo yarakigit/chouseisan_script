c2f657fdda2d66cf11fc71fd7bdfe152e2d1c7cc
yarakigit <ya.tech.go@gmail.com>
2022-07-07 01:45:26 +0900

    add exclusion date and hour

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 import csv
 import datetime
 
+week_dict = {
+     0:'Monday',
+     1:'Tuesday',
+     2:'Wednesday',
+     3:'Thursday',
+     4:'Friday',
+     5:'Saturday',
+     6:'Sunday'
+}
+
 def main():
     in_file_name = '../setting.csv'
     out_file_name = '../output.txt'
@@ -13,90 +23,107 @@ def main():
         while (cal_date - info.datetime_finish).days <= 0:
             cal_time = info.time_start
             while info.time_finish>=cal_time:
-                cal_week = cal_date.strftime('%A')
-                if judge_week(cal_week,info.week_info):
-                    output_str += make_out_file_str(cal_date.month,cal_date.day,week_jp(cal_week),cal_time)
+                exclusion_date_flag = False
+                for exclusion_date in info.exclusion_date_list:
+                    if exclusion_date.month==cal_date.month and exclusion_date.day==cal_date.day: 
+                        exclusion_date_flag = True
+                exclusion_time_flag = False
+                for exclusion_time in info.exclusion_time_list:
+                    if exclusion_time==cal_time:
+                        exclusion_time_flag = True
+                if not (exclusion_date_flag or exclusion_time_flag):
+                    cal_week = cal_date.strftime('%A')
+                    if judge_week(cal_week,info.week_info):
+                        output_str += make_out_file_str(cal_date.month,cal_date.day,week_jp(cal_week),cal_time)
                 cal_time += info.time_span
-
             cal_date += datetime.timedelta(days=1)
-        
         fw.write(output_str.rstrip('\n'))
 
 def judge_week(cal_week,week_info):
-    if week_info.monday and cal_week == 'Monday':
+    if week_info.Monday and cal_week == week_dict[0]:
         return True
-    elif week_info.tuesday and cal_week == 'Tuesday':
+    elif week_info.Tuesday and cal_week == week_dict[1]:
         return True
-    elif week_info.wednesday and cal_week == 'Wednesday':
+    elif week_info.Wednesday and cal_week == week_dict[2]:
         return True
-    elif week_info.thursday and cal_week == 'Thursday':
+    elif week_info.Thursday and cal_week == week_dict[3]:
         return True
-    elif week_info.friday and cal_week == 'Friday':
+    elif week_info.Friday and cal_week == week_dict[4]:
         return True
-    elif week_info.saturday and cal_week == 'Saturday':
+    elif week_info.Saturday and cal_week == week_dict[5]:
         return True
-    elif week_info.sunday and cal_week == 'Sunday':
+    elif week_info.Sunday and cal_week == week_dict[6]:
         return True
     
 def week_jp(week_eng):
-    if week_eng=='Monday':
+    if week_eng==week_dict[0]:
         return '月'
-    elif week_eng=='Tuesday':
+    elif week_eng==week_dict[1]:
         return '火'
-    elif week_eng=='Wednesday':
+    elif week_eng==week_dict[2]:
         return '水'
-    elif week_eng=='Thursday':
+    elif week_eng==week_dict[3]:
         return '木'
-    elif week_eng=='Friday':
+    elif week_eng==week_dict[4]:
         return '金'
-    elif week_eng=='Saturday':
+    elif week_eng==week_dict[5]:
         return '土'
-    elif week_eng=='Sunday':
+    elif week_eng==week_dict[6]:
         return '日'
 def make_out_file_str(month,day,week,time):
     return "{month}/{day}({week}) {time}:00~\n".format(month=month, day=day, week=week, time=time)
     
 def extract_csv(reader_csv):
     week_info = week()
+    exclusion_date_list = []
+    exclusion_time_list = []
     for i,row in enumerate(reader_csv):
-        if row[0] == 'year':
+        if len(row) == 0:
+            break
+        elif row[0] == 'year':
             year = int(row[1]) 
         elif row[0] == 'start date':
             start_date = date(int(row[1]),int(row[2]))
         elif row[0] == 'finish date':
             finish_date = date(int(row[1]),int(row[2]))
-        elif row[0] == 'monday':
+        elif row[0] == week_dict[0]:
             if row[1]=='1':
-                week_info.monday = True
-        elif row[0] == 'tuesday':
+                week_info.Monday = True
+        elif row[0] == week_dict[1]:
             if row[1]=='1':
-                week_info.tuesday = True
-        elif row[0] == 'wednesday':
+                week_info.Tuesday = True
+        elif row[0] == week_dict[2]:
             if row[1]=='1':
-                week_info.wednesday = True
-        elif row[0] == 'thursday':
+                week_info.Wednesday = True
+        elif row[0] == week_dict[3]:
             if row[1]=='1':
-                week_info.thursday = True
-        elif row[0] == 'friday':
+                week_info.Thursday = True
+        elif row[0] == week_dict[4]:
             if row[1]=='1':
-                week_info.friday = True
-        elif row[0] == 'saturday':
+                week_info.Friday = True
+        elif row[0] == week_dict[5]:
             if row[1]=='1':
-                week_info.saturday = True
-        elif row[0] == 'sunday':
+                week_info.Saturday = True
+        elif row[0] == week_dict[6]:
             if row[1]=='1':
-                week_info.sunday = True
+                week_info.Sunday = True
         elif row[0] == 'time start':
             time_start = int(row[1])
         elif row[0] == 'time finish':
             time_finish = int(row[1])
         elif row[0] == 'time span':
             time_span = int(row[1])
-    out_info = info(year,start_date,finish_date,week_info,time_start,time_finish,time_span)
+        elif row[0] == 'exclusion date':
+            if not row[1] == 'None':
+                exclusion_date_list.append(date(int(row[1]),int(row[2])))
+        elif row[0] == 'exclusion time':
+            if not row[1] == 'None':
+                exclusion_time_list.append(int(row[1]))
+    out_info = info(year,start_date,finish_date,week_info,time_start,time_finish,time_span,exclusion_date_list,exclusion_time_list)
     
     return out_info
 class info:
-    def __init__(self,year,start_date,finish_date,week_info,time_start,time_finish,time_span):
+    def __init__(self,year,start_date,finish_date,week_info,time_start,time_finish,time_span,exclusion_date_list,exclusion_time_list):
         self.year = year
         self.start_date = start_date
         self.finish_date = finish_date
@@ -106,9 +133,13 @@ class info:
         self.time_span = time_span
         self.datetime_start = datetime.datetime(year=year,month=start_date.month,day=start_date.day) + datetime.timedelta(hours=9) 
         self.datetime_finish = datetime.datetime(year=year,month=finish_date.month,day=finish_date.day) + datetime.timedelta(hours=9) 
+        self.exclusion_date_list = exclusion_date_list
+        self.exclusion_time_list = exclusion_time_list
     def print_info(self):
         print("year:",self.year)
+        print("start_date : ",end='')
         self.start_date.print_info()
+        print("finish_date : ",end='')
         self.finish_date.print_info()
         self.week_info.print_info()
         print("time_start:",self.time_start)
@@ -116,8 +147,13 @@ class info:
         print("time_span:",self.time_span)
         print("datetime_start",self.datetime_start)
         print("datetime_finish",self.datetime_finish)
-
-
+        for exclusion_date in self.exclusion_date_list:
+            print("exclusion_date : ",end='')
+            exclusion_date.print_info()
+        for exclusion_time in self.exclusion_time_list:
+            print("exclusion_time : ",end='')
+            print(exclusion_time)
+        
 class date:
     def __init__(self,month,day):
         self.month = month
@@ -128,21 +164,22 @@ class date:
 
 class week:
     def __init__(self):
-        self.monday = False
-        self.tuesday = False
-        self.wednesday = False
-        self.thursday = False
-        self.friday = False
-        self.saturday = False
-        self.sunday = False
+        self.Monday = False
+        self.Tuesday = False
+        self.Wednesday = False
+        self.Thursday = False
+        self.Friday = False
+        self.Saturday = False
+        self.Sunday = False
     def print_info(self):
-        print("monday   :",self.monday)
-        print("tuesday  :",self.tuesday)
-        print("wednesday:",self.wednesday)
-        print("thursday :",self.thursday)
-        print("friday   :",self.friday)
-        print("saturday :",self.saturday)
-        print("sunday   :",self.sunday)
+        print('{week:<9}: {week_en}'.format(week=week_dict[0],week_en=self.Monday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[1],week_en=self.Tuesday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[2],week_en=self.Wednesday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[3],week_en=self.Thursday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[4],week_en=self.Friday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[5],week_en=self.Saturday))
+        print('{week:<9}: {week_en}'.format(week=week_dict[6],week_en=self.Sunday))
+        
         
 
 main()
